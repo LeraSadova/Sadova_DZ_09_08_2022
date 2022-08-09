d33724f7bd0ccdce917823d8f318f9a1f60621bb
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-09 16:32:27 +0300

    решила 13ю задачу

--- Z13_vivod_3_chisla/Program.cs
+++ Z13_vivod_3_chisla/Program.cs
@@ -2,3 +2,18 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+//Console.Clear();
+Console.WriteLine("Введите любое число, чтобы потом вывести третью цифру этого числа ");
+Console.Write("Ваше число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string numberText = Convert.ToString(number); //Convert.ToString() функция, которая для числа даёт его строковое представление.
+
+if (numberText.Length > 2)
+    {
+        Console.WriteLine($"Третья цифра числа '{number}' -> " + numberText[2]);
+    }
+else 
+    {
+        Console.WriteLine($"В введенном числе '{number}' третьей цифры нет");
+    }
